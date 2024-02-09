300ed539a6101ac9c4bf31e3ea3099fba9393eb9
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-02-10 04:13:40 +0530

    Fixed Footer issues for home page.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-24 bg-mainBlue text-9xl py-24 text-white font-bold flex items-center justify-center">
+    <div className="h-24 bg-mainBlue text-4xl sm:text-9xl py-24 text-white font-bold flex items-center justify-center">
       Footer
     </div>
   );
